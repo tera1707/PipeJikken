6e4888058120b2374e63d15aae837489b587aa91
tera1707 <Masa1017>
2026-04-12 20:19:21 +0900

    サービスのstartstopのバッチを追加

---
+++ StopService.bat
@@ -0,0 +1,30 @@
+@set @dummy=0/*
+@echo off
+NET FILE 1>NUL 2>NUL
+if "%ERRORLEVEL%" neq "0" (
+  cscript //nologo //E:JScript "%~f0" %*
+  exit /b %ERRORLEVEL%
+)
+
+REM 管理者権限で実行したい処理 ここから
+
+cd %~dp0
+
+set SERVICENAME="ServiceJikkenSvcName"
+set SERVICEEXENAME=ServiceJikken.exe
+set SERVICEDISPNAME=%SERVICENAME%
+set BINPATH=%~dp0x64\Debug\%SERVICEEXENAME%
+set DISCRIPTION="Service Jikken Svc Description..."
+
+REM サービスを止める
+net stop %SERVICENAME%
+
+pause
+
+REM 管理者権限で実行したい処理 ここまで
+
+goto :EOF
+*/
+var cmd = '"/c ""' + WScript.ScriptFullName + '" ';
+for (var i = 0; i < WScript.Arguments.Length; i++) cmd += '"' + WScript.Arguments(i) + '" ';
+(new ActiveXObject('Shell.Application')).ShellExecute('cmd.exe', cmd + ' "', '', 'runas', 1);
